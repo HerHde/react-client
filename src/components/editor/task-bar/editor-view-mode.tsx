60cf0b5b3afefda5baf4c465ab6ae6c3a0e0ad09
Philip Molares <philip.molares@udo.edu>
2020-05-20 11:44:10 +0200

    editor-view-mode component correctly displays and sets state
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/components/editor/task-bar/editor-view-mode.tsx
+++ src/components/editor/task-bar/editor-view-mode.tsx
@@ -1,17 +1,27 @@
 import {ToggleButton, ToggleButtonGroup} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React from "react";
+import {useSelector} from "react-redux";
+import {ApplicationState} from "../../../redux";
+import {EditorMode} from "../../../redux/editor/types";
+import {setEditorModeConfig} from "../../../redux/editor/methods";
 
 const EditorViewMode: React.FC = () => {
+    const editorConfig = useSelector((state: ApplicationState) => state.editorConfig);
     return (
-        <ToggleButtonGroup type="radio" name="options" defaultValue={1} className="pull-left button-middle">
-            <ToggleButton value={1} variant="secondary" size="sm">
+        <ToggleButtonGroup
+            type="radio"
+            name="options"
+            defaultValue={editorConfig.editorMode}
+            className="pull-left button-middle"
+            onChange={(value: EditorMode) => {setEditorModeConfig(value)}}>
+            <ToggleButton value={EditorMode.PREVIEW} variant="secondary" size="sm">
                 <FontAwesomeIcon icon="eye"/>
             </ToggleButton>
-            <ToggleButton value={2} variant="secondary" size="sm">
+            <ToggleButton value={EditorMode.BOTH} variant="secondary" size="sm">
                 <FontAwesomeIcon icon="columns"/>
             </ToggleButton>
-            <ToggleButton value={3} variant="secondary" size="sm">
+            <ToggleButton value={EditorMode.EDITOR} variant="secondary" size="sm">
                 <FontAwesomeIcon icon="pencil-alt"/>
             </ToggleButton>
         </ToggleButtonGroup>
