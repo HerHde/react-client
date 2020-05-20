c8837a1b98bf59fab3bb229283be521a84d10786
Philip Molares <philip.molares@udo.edu>
2020-05-20 11:43:33 +0200

    added editor config redux state
    Signed-off-by: Philip Molares <philip.molares@udo.edu>

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -7,17 +7,21 @@ import {BackendConfigState} from "./backend-config/types";
 import {FrontendConfigState} from "./frontend-config/types";
 import {BackendConfigReducer} from "./backend-config/reducers";
 import {FrontendConfigReducer} from "./frontend-config/reducers";
+import {EditorConfigState} from "./editor/types";
+import {EditorConfigReducer} from "./editor/reducers";
 
 export interface ApplicationState {
     user: UserState;
     modalShow: ModalShowState;
     backendConfig: BackendConfigState;
     frontendConfig: FrontendConfigState;
+    editorConfig: EditorConfigState;
 }
 
 export const allReducers: Reducer<ApplicationState> = combineReducers<ApplicationState>({
     user: UserReducer,
     modalShow: ModalShowReducer,
     backendConfig: BackendConfigReducer,
-    frontendConfig: FrontendConfigReducer
+    frontendConfig: FrontendConfigReducer,
+    editorConfig: EditorConfigReducer
 });
